957697bfff2efaaf096e77f6764f377a2dc13b63
William Niemiec <williamniemiec@hotmail.com>
2022-06-10 14:32:43 -0300

    Refactoring

--- ios/nfitTests/nfitTests.m
+++ ios/nfitTests/nfitTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ntrainingTests : XCTestCase
+@interface nfitTests : XCTestCase
 
 @end
 
-@implementation ntrainingTests
+@implementation nfitTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
